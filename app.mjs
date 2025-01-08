50889a7b4aecfcda30ec2039ce1b274d08cc361b
runaround <runaroundontheground@gmail.com>
2025-01-08 10:17:59 -0700

    learned state stuff

--- app.mjs
+++ app.mjs
@@ -1,64 +1,81 @@
 import express from "express";
+import session from "express-session";
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
 
 const port = 3000;
 const app = express();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const users = [];
+app.use(
+  session({
+    secret: "very secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 10000 },
+  })
+);
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "static")));
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/", (req, res) => {
-//   res.send(`<h1>hello</h1>`);
-// });
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-// app.get("/htmltest", (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
-app.get("/time", (req, res) => {
-  res.send(new Date().toString());
-});
-app.get("/texttest", (req, res) => {
-  res.sendFile(path.join(__dirname, "text.txt"));
-});
-app.get("/test", (req, res) => {
-  // const file = fs.readFileSync("text.txt", "utf8");
-  const file = fs.readFileSync("index.html", "utf8");
-  res.send(file);
-});
-app.post("/addUser", (req, res) => {
-  const firstName = req.body.firstName;
-  const { email, lastName } = req.body;
-  const newUser = { email, firstName, lastName };
-  users.push(newUser);
-  console.log(users);
-  res.send(`email: ${email}, first name: ${firstName}, last name: ${lastName}`);
-});
-app.get("/findUser", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-app.post("/findUser", (req, res) => {
-  const { email } = req.body;
-  for (let i = 0; i < users.length; i++) {
-    // assuming users length won't change while this is running
-    if (users[i].email === email) {
-      res.send(
-        `<h3>found user ${users[i].firstName} ${users[i].lastName}, ${users[i].email}`
-      );
-      return;
-    }
+app.use(
+  (req, res, next) => {
+    // this should run any time a request is sent to the server
+    res.cookie("acookie", "myvalue2", { httpOnly: true, maxAge: 10000 });
+
+    // console.log("test");
+    next(); // prevents the client from hanging on a response from the server in this function
+  },
+  (req, res, next) => {
+    // console.log("next in the chain");
+    next();
   }
-  res.send(`<h3>no user found</h3>`);
+);
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    // this should run any time a request is sent to the server
+    console.log("thing");
+    next(); // prevents the client from hanging on a response from the server in this function
+  },
+  (req, res, next) => {
+    console.log("next in the chain of thing");
+    next();
+  }
+);
+
+// app.get("/exit", (req, res) => {
+//   server.close();
+//   res.end();
+// })
+
+app.get("/test", (req, res) => {
+  if (req.session.timesPageViewed) {
+    req.session.timesPageViewed++;
+    res.setHeader("Content-type", "text/html");
+    res.write(`<p>page viewed ${req.session.timesPageViewed} times</p>`);
+    res.write(
+      `<p>session expires in ${req.session.cookie.maxAge / 1000} seconds</p>`
+    );
+    res.end();
+  } else {
+    req.session.timesPageViewed = 1;
+    res.end("test");
+  }
 });
 
-app.listen(port, () => {
+app.get("/", (req, res) => {
+  res.cookie("mycookie", "myvalue", { httpOnly: true, maxAge: 10000 });
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.send("<h1>hello world</h1>");
+});
+
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
